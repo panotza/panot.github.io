57b9d9c210e1553be07ded26455c986ec6884088
panotza <panotza_ja@hotmail.com>
2018-05-13 21:04:53 +0700

    init project

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const gulp = require('gulp')
+const rename = require('gulp-rename')
+const uglify = require('gulp-uglify')
+const csso = require('gulp-csso')
+const del = require('del')
+const htmlmin = require('gulp-htmlmin')
+const runSequence = require('run-sequence')
+
+const DEST = './dist/'
+
+gulp.task('scripts', () => {
+  return gulp.src('./script.js')
+    .pipe(uglify())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(gulp.dest(DEST))
+})
+
+// Gulp task to minify HTML files
+gulp.task('pages', () => {
+  return gulp.src(['./index.html'])
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      removeComments: true
+    }))
+    .pipe(gulp.dest(DEST))
+})
+
+// Gulp task to minify CSS files
+gulp.task('styles', function () {
+  return gulp.src('./styles.css')
+    .pipe(csso())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(gulp.dest(DEST))
+})
+
+// Clean output directory
+gulp.task('clean', () => del([DEST]))
+
+// Gulp task to minify all files
+gulp.task('default', ['clean'], () => {
+  runSequence(
+    'styles',
+    'scripts',
+    'pages'
+  )
+})
