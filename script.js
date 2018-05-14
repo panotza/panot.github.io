0620ceb96b999cb9e709aafa51fdb0e083711069
panotza <panotza_ja@hotmail.com>
2018-05-14 12:50:58 +0700

    refactor and add some basic commands

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.addEventListener('keydown', getKey)
 
 const terminal = document.getElementById('terminal')
+let lastLine = terminal.lastElementChild
 let input = ''
 
 function getKey (e) {
   parseKey(e.key)
-  terminal.lastElementChild.innerHTML = input + '<span></span>'
   updateScroll()
 }
 
@@ -16,10 +16,8 @@ function updateScroll () {
 function parseKey (key) {
   switch (key) {
     case 'Enter':
-      const newLine = document.createElement('div')
-      const oldLine = terminal.lastElementChild.innerHTML
-      terminal.lastElementChild.innerHTML = oldLine.slice(0, oldLine.length - '<span></span>'.length)
-      terminal.appendChild(newLine)
+      const inputArgs = input.split(' ')
+      parseCmd(...inputArgs)
       input = ''
       break
     case 'Backspace':
@@ -28,4 +26,55 @@ function parseKey (key) {
     default:
       input = input + key
   }
+  lastLine.innerHTML = input + '<span></span>'
+}
+
+function parseCmd (cmd, ...args) {
+  console.log(args)
+  switch (cmd) {
+    case 'help':
+      output(`
+        <div class="help">
+          <div>ls</div><div>list directory contents</div>
+          <div>cat</div><div>concatenate and print files</div>
+        </div>
+      `)
+      break
+    case 'ls':
+      output('profile contact projects')
+      break
+    case 'cat':
+      switch (args[0]) {
+        case 'profile':
+          output('name: Panot Wongkhot')
+          break
+        case 'contact':
+          output('panot.wongkhot@gmail.com')
+          break
+        default:
+          output('file not found.')
+      }
+      break
+    case '':
+      println('')
+      break
+    default:
+      output('Unknow command')
+  }
+}
+
+function println (msg, isOutput = false) {
+  lastLine.innerHTML = lastLine.innerHTML.replace('<span></span>', '')
+  let newLine = document.createElement('div')
+  newLine.innerHTML = msg
+  if (isOutput) {
+    newLine.className = 'output'
+  }
+  terminal.appendChild(newLine)
+  lastLine = newLine
+}
+
+function output (msg) {
+  println(msg, true)
+  println('')
 }
