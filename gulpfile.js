38714a3a0e08df42c5b11ca79c98ad3ab4497db3
panotza <panotza_ja@hotmail.com>
2018-05-14 11:20:47 +0700

    update gulpfile wth rev, rev-replace

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,21 @@ const csso = require('gulp-csso')
 const del = require('del')
 const htmlmin = require('gulp-htmlmin')
 const runSequence = require('run-sequence')
+const rev = require('gulp-rev')
+const revReplace = require('gulp-rev-replace')
+const path = require('path')
 
-const DEST = './dist/'
+const DEST = path.join(__dirname, 'dist')
 
 gulp.task('scripts', () => {
   return gulp.src('./script.js')
     .pipe(minify())
+    .pipe(rev())
+    .pipe(gulp.dest(DEST))
+    .pipe(rev.manifest({
+      cwd: DEST,
+      merge: true // merge with the existing manifest if one exists
+    }))
     .pipe(gulp.dest(DEST))
 })
 
@@ -29,6 +38,16 @@ gulp.task('pages', () => {
 gulp.task('styles', function () {
   return gulp.src('./styles.css')
     .pipe(csso())
+    .pipe(rev())
+    .pipe(gulp.dest(DEST))
+    .pipe(rev.manifest())
+    .pipe(gulp.dest(DEST))
+})
+
+gulp.task('revreplace', () => {
+  const manifest = gulp.src(path.join(DEST, 'rev-manifest.json'))
+  return gulp.src(path.join(DEST, 'index.html'))
+    .pipe(revReplace({manifest: manifest}))
     .pipe(gulp.dest(DEST))
 })
 
@@ -40,6 +59,7 @@ gulp.task('default', ['clean'], () => {
   runSequence(
     'styles',
     'scripts',
-    'pages'
+    'pages',
+    'revreplace'
   )
 })
